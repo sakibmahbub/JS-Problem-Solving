bf226e676fb31f0c0a81a4656ec0ea9526e00af0
sakibmahbub <arafatsakib48@gmail.com>
2023-07-27 21:13:00 +0600

    solved task 7

---
+++ task7.js
@@ -0,0 +1,31 @@
+function romanToInteger(s) {
+  const romanToInt = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+  let previousValue = 0;
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    const currentChar = s[i];
+    const currentValue = romanToInt[currentChar];
+
+    if (currentValue >= previousValue) {
+      total += currentValue;
+    } else {
+      total -= currentValue;
+    }
+
+    previousValue = currentValue;
+  }
+
+  return total;
+}
+
+console.log(romanToInteger("IV"));
